36482beeb8c28ec7fdcc0415ae8a6a0f19018b1e
DSastreHernandez <dsastre@cifpfbmoll.eu>
2022-01-27 18:03:49 +0100

    test: :zap: Pasar de string a matriz ordenada

--- Bowling/Tabla_ordenada.py
+++ Bowling/Tabla_ordenada.py
@@ -1,31 +1,44 @@
-class Tabla:    
-    
-    def __init__(self,puntos = ""):
-        self.tabla_ordenada = []
-        self.puntos = puntos
+class Tabla():   
 
-    def set_tabla_ordenada(self):
-        puntos = self.puntos
-        tabla_ordenada = []
+    def __init__(self,puntos = ""):
+        self.tabla = []
+        self.puntos = puntos
+        self.tabla_limpia = []
+
+    def set_tabla(self):
+        puntos = self.tabla_limpia
         tabla_turno = []
         for element in puntos:
             if len(tabla_turno) < 2:
                 turno = element
                 tabla_turno.append(turno)
             else:
-                tabla_ordenada.append(tabla_turno)
+                self.tabla.append(tabla_turno)
                 tabla_turno = [element]
         if len(tabla_turno) == 1:
-            tabla_ordenada[-1] += tabla_turno
+            self.tabla[-1].append(tabla_turno[0])
         elif len(tabla_turno) == 2:
-            tabla_ordenada.append(tabla_turno)
-        self.tabla_ordenada = tabla_ordenada
+            self.tabla.append(tabla_turno)
 
-    def get_tabla_ordenada(self):
-        return self.tabla_ordenada
+    def get_tabla(self):
+        return self.tabla
+
+    def set_tabla_limpia(self):
+        for element in self.puntos:
+            try:
+                if int(element):
+                    self.tabla_limpia.append(int(element))
+            except:
+                self.tabla_limpia.append(element)
 
 if __name__ == '__main__':
 
-    Tabla("9-9-9-9-9-9-9-9-9-9-")
-    Tabla.set_tabla_ordenada()
-    assert Tabla.get_tabla_ordenada() == [[9,'-'], [9,'-'], [9,'-'], [9,'-'], [9,'-'], [9,'-'], [9,'-'], [9,'-'], [9,'-'], [9,'-']] 
+    tabla = Tabla("9-9-9-9-9-9-9-9-9-9-")
+    tabla.set_tabla_limpia()
+    tabla.set_tabla()
+    assert tabla.get_tabla() == [[9,'-'], [9,'-'], [9,'-'], [9,'-'], [9,'-'], [9,'-'], [9,'-'], [9,'-'], [9,'-'], [9,'-']] 
+
+    tabla = Tabla("5/5/5/5/5/5/5/5/5/5/5")  
+    tabla.set_tabla_limpia()
+    tabla.set_tabla()
+    assert tabla.get_tabla() == [[5,'/'],[5,'/'],[5,'/'],[5,'/'],[5,'/'],[5,'/'],[5,'/'],[5,'/'],[5,'/'],[5,'/',5]]
