537361141637314d0b200eafdb5c104cc79dc758
DSastreHernandez <dsastre@cifpfbmoll.eu>
2022-01-27 17:04:55 +0100

    feat: :sparkles: Clase Bowling acabada
    Clase Bowling acabada la cual da la puntuacion de la partida, queda pasar la Score card de una string a una matriz ordenada con la puntuacion para acabar el proyecto

--- Bowling/Bowling.py
+++ Bowling/Bowling.py
@@ -1,50 +1,113 @@
 class Bowling:
 
-    def __init__(self,puntos=""):
-        self.tabla_ordenada = []
+    ## Globals ##
+    STRIKE = 'X'
+    SPARE = '/'
+    NO_PINS_BOWLED = '-'
+
+    def __init__(self,tabla=[]):
         self.puntuacion = 0
-        self.puntos = puntos
-
-    def ordenar_tabla(self):
-        Bowling.sanear_putnos(self)
-        puntos = self.puntos
-        tabla_ordenada = []
-        tabla_turno = []
-        for element in puntos:
-            if len(tabla_turno) < 2:
-                tabla_turno.append(element)
-            else:
-                tabla_ordenada.append(tabla_turno)
-                tabla_turno = [element]
-        if len(tabla_turno) == 1:
-            tabla_ordenada[-1] += tabla_turno
-        elif len(tabla_turno) == 2:
-            tabla_ordenada.append(tabla_turno)
-        self.tabla_ordenada = tabla_ordenada
-
-
+        self.tabla = tabla
             
+    def set_puntuacion(self):
+        num_turno = 0
+        while num_turno + 1 <= len(self.tabla):
+            puntos_turno = 0
 
-    def get_tabla_ordenada(self):
-        Bowling.ordenar_tabla(self)
-        return self.tabla_ordenada
+            for tirada in self.tabla[num_turno]:
+            
+                if isinstance(tirada, int):
+                    puntos_turno += tirada
+            
+                elif tirada == self.STRIKE:
+                    puntos_turno += self.__strike(num_turno)
+            
+                elif tirada == self.SPARE:
+                    puntos_turno = self.__spare(num_turno)
 
-    def sanear_putnos(self):
-        puntos = self.puntos
-        puntos_saneados = []
-        for element in puntos:
-            try:
-                puntos_saneados.append(int(element))
-            except:
-                puntos_saneados.append(element)
-        self.puntos = puntos_saneados
+            self.puntuacion += puntos_turno
+            num_turno += 1
+
+    def __spare(self,num_turno):
+        try:
+            return 10 + self.tabla[num_turno + 1][0]
+        except:
+            return 10  
+
+    def __strike(self,num_turno):
+        try:
+            if len(self.tabla[num_turno + 1]) >= 2:
+                return  10 +  self.__puntos_netos_turno(self.tabla[num_turno + 1])
+            else:
+                return 10 + self.__puntos_netos_turno(self.tabla[num_turno + 1]) + self.__puntos_netos_tiro(self.tabla[num_turno + 2][0])
+        except:
+            return 10
+
+
+        
+    def __puntos_netos_turno(self,turno):
+        if turno[0] == self.STRIKE:
+            return 10
+        elif turno[-1] == self.SPARE:
+            return 10
+        else:
+            return sum(filter(lambda tiro: isinstance(tiro, int),turno))
+
+    def __puntos_netos_tiro(self,tiro):
+        if tiro == self.STRIKE:
+            return 10
+        elif tiro == self.NO_PINS_BOWLED:
+            return 0
+        else:
+            return tiro
+
+
+
+
+    def get_puntuacion(self):
+        return self.puntuacion 
+
                 
 
 
 if __name__ == '__main__':
 
-    bowling = Bowling('11111111111111111111')
-    assert [[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]] == bowling.get_tabla_ordenada()
-
-    bowling = Bowling('1/111111111111111111')
+    bowling = Bowling([[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]])
+    bowling.set_puntuacion()
+    assert  20 ==  bowling.get_puntuacion()
     
+    bowling = Bowling([[9,0],[9,0],[9,0],[9,0],[9,0],[9,0],[9,0],[9,0],[9,0],[9,0]])
+    bowling.set_puntuacion()
+    assert 90 == bowling.get_puntuacion()
+
+    bowling = Bowling([[5,'/'],[5,'/'],[5,'/'],[5,'/'],[5,'/'],[5,'/'],[5,'/'],[5,'/'],[5,'/'],[5,'/',5]])
+    bowling.set_puntuacion()
+    assert 150 == bowling.get_puntuacion()
+
+    bowling = Bowling([[9,'-'],[3,'/'],[6,1],[3,'/'],[8,1],[5,'/'],['-','/'],[8,'-'],[7,'/'],[8,'/',8]])
+    bowling.set_puntuacion()
+    assert 131 == bowling.get_puntuacion()
+
+    bowling = Bowling([['X'],[9,'-'],[9,'-'],[9,'-'],[9,'-'],[9,'-'],[9,'-'],[9,'-'],[9,'-'],[9,'-']])
+    bowling.set_puntuacion()
+    assert 100 == bowling.get_puntuacion()
+
+    bowling = Bowling([['X'],['X'],[8,'-'],[4,4],[4,4],[4,4],[4,4],[4,4],[4,4],[4,4]])
+    bowling.set_puntuacion()
+    assert 110 == bowling.get_puntuacion()
+
+    bowling = Bowling([['X'],['X'],[9,'-'],[9,'-'],[9,'-'],[9,'-'],[9,'-'],[9,'-'],[9,'-'],[9,'-']])
+    bowling.set_puntuacion()
+    assert 120 == bowling.get_puntuacion()
+
+    bowling = Bowling([['X'],['X'],['X'],[9,'-'],[9,'-'],[9,'-'],[9,'-'],[9,'-'],[9,'-'],[9,'-']])
+    bowling.set_puntuacion()
+    assert 141 == bowling.get_puntuacion()
+
+    bowling = Bowling([[9,'-'],[9,'-'],[9,'-'],[9,'-'],[9,'-'],[9,'-'],[9,'-'],[9,'-'],[9,'-'],['X','X','X']])
+    bowling.set_puntuacion()
+    assert 111 == bowling.get_puntuacion()
+
+    bowling = Bowling([['X'],['X'],[8,'-'],[4,4],[4,4],[4,4],[4,4],[4,4],[4,4],[4,4]])
+    bowling.set_puntuacion()
+    assert 110 == bowling.get_puntuacion()
--- Bowling/Tabla_ordenada.py
+++ Bowling/Tabla_ordenada.py
@@ -4,7 +4,7 @@ class Tabla:
         self.tabla_ordenada = []
         self.puntos = puntos
 
-    def ordenar_tabla(self):
+    def set_tabla_ordenada(self):
         puntos = self.puntos
         tabla_ordenada = []
         tabla_turno = []
@@ -21,9 +21,11 @@ class Tabla:
             tabla_ordenada.append(tabla_turno)
         self.tabla_ordenada = tabla_ordenada
 
-
-            
-
     def get_tabla_ordenada(self):
-        Tabla.ordenar_tabla(self)
         return self.tabla_ordenada
+
+if __name__ == '__main__':
+
+    Tabla("9-9-9-9-9-9-9-9-9-9-")
+    Tabla.set_tabla_ordenada()
+    assert Tabla.get_tabla_ordenada() == [[9,'-'], [9,'-'], [9,'-'], [9,'-'], [9,'-'], [9,'-'], [9,'-'], [9,'-'], [9,'-'], [9,'-']] 
